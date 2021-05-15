d4636c2ef8f66377ece57921221d41a8930c9c1b
Shubhamsk1 <shubahmsk1@gmail.com>
2021-05-16 00:28:54 +0530

    Cycle Detection using Dfs added

---
+++ cycleDetectionDFS.cpp
@@ -0,0 +1,93 @@
+/* Code by shubhamsk1 */
+/* Shubham S Kudekar */
+#include<bits/stdc++.h>
+#define int long long
+#define for0(i, n) for (int i = 0; i < n; i++)
+#define for1(i, n) for (int i = 1; i <= n; i++)
+#define endl "\n"
+#define N 10000007
+using namespace std;
+int fme(int a,int b){
+int res=1;
+while(b>0){
+if(b&1){
+res=(res*a)%N;
+}
+a=(a*a)%N;
+b=b>>1;
+}
+return res;
+}
+/*Declare function prototypes here*/
+void solve();
+
+
+
+/*Declare Global variables here*/
+
+
+
+/* Main function starts from here */
+int32_t main() { 
+/*Fast IO*/ 
+ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+/*Input-output file creation*/
+#ifndef ONLINE_JUDGE 
+freopen("input.txt", "r", stdin); 
+freopen("error.txt", "w", stderr); 
+freopen("output.txt", "w", stdout);
+#endif
+
+int t=1; 
+
+/*is Single Test case?*/cin>>t;
+while(t--)
+{ 
+solve(); 
+cout<<"\n";
+}
+cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC*1000<<" ms"<<endl;
+
+return 0;
+}
+/*Main function Ends */
+bool checkCycle(int node , int parent, int n , vector<int> adj[],vector<int>& vis){
+    vis[node]=1;
+    for(auto j: adj[node]){
+        if(!vis[j]){
+            if(checkCycle(j,node,n,adj,vis)) return true;
+        }
+        else if(j!=parent) return true;
+    }
+    return false;
+
+}
+
+bool detectCycle(int n,vector<int> adj[]){
+    vector<int> vis(n,0);
+    for(int i=0;i<n;i++){
+        if(!vis[i]){
+            if(checkCycle(i,-1,n,adj,vis)) return true;
+        }
+    }
+    return false;
+}
+
+/*Write all function Definition Here */
+void solve() { 
+    int n,m;
+    cin>>n>>m;
+    vector<int> adj[n];
+    
+    for0(i,m){
+        int x,y;
+        cin>>x>>y;
+        adj[x-1].push_back(y-1);
+        adj[y-1].push_back(x-1);
+    }
+    if(detectCycle(n,adj)) cout<<"Cycle Detected";
+    else cout<<"Cycle absent";
+    
+
+}
